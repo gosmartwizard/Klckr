96a884ca230e5fe517cd26a35a5981c1fd4ec87b
gosmartwizard <gosmartwizard@gmail.com>
2023-07-30 10:04:53 -0400

    Added the http request url path

--- main.go
+++ main.go
@@ -13,11 +13,16 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:nrkr@klckr.io\">nrkr@klckr.io</a>.</p>")
-
 }
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, r.URL.Path)
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	//http.HandleFunc("/", homeHandler)
+	//http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Server will listen on port : 4949")
 	http.ListenAndServe(":4949", nil)
 }

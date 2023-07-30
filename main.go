d019250d63d297338ece8520f8d348475b33cbe0
gosmartwizard <gosmartwizard@gmail.com>
2023-07-30 09:51:55 -0400

    Added the Contact page handler

--- main.go
+++ main.go
@@ -5,13 +5,19 @@ import (
 	"net/http"
 )
 
-func BasicHandler(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to the world of Klckr</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:nrkr@klckr.io\">nrkr@klckr.io</a>.</p>")
+
+}
 func main() {
-	http.HandleFunc("/", BasicHandler)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Server will listen on port : 4949")
 	http.ListenAndServe(":4949", nil)
 }
